298baaeeae94128c5856db83f0313c7849e7ea2e
CentroDL <dennis.liaw@gmail.com>
2015-03-08 11:00:36 -0400

    adds rope to level

--- scripts/platform.js
+++ scripts/platform.js
@@ -6,12 +6,16 @@ function preload() {
 
     game.load.spritesheet('dude', 'assets/images/dude.png', 18, 24, 23, 0, 14 );
     //spritesheet(key, url, frameWidth, frameHeight, frameMax, margin, spacing)
+    // game.load.tilemap('godot-tree', 'assets/godot.json', null, Phaser.Tilemap.TILED_JSON);
+
     game.load.image('background', 'assets/layers/parallax-mountain-bg.png');
-    game.load.image('tiles-1', 'assets/images/sheet_9.png');
+    game.load.image('chain', 'assets/images/chain.png', 16, 16);
+    // game.load.image('tiles-1', 'assets/images/sheet_9.png');
 
 }
 
 var player;
+var chain;
 var facing = 'right';
 var jumpTimer = 0;
 var cursors;
@@ -27,11 +31,13 @@ function create() {
     bg.fixedToCamera = true;
 
     //level
-    map = game.add.tilemap( Phaser.Tilemap.create('blocks', 64, 64, 16, 16) );
-    map.addTileSetImage('tiles-1');
-    layer = map.createLayer('Tile Layer 1');
+    // map = game.add.tilemap( 'godot-tree' );
+    // map.addTilesetImage('tiles-1');
+    // layer = map.createLayer('Tile Layer 1');
+    // layer.resizeWorld();
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.arcade.gravity.y = 300;
 
     // add.sprite x-coord, y-coord, flag, frame
@@ -49,7 +55,7 @@ function create() {
     // player.body.setSize(20, 32, 5, 16);
 
 
-    player.animations.add('walk', [ 1, 2, 3, 4], 10, true);
+    player.animations.add('walk', [ 1, 2, 3, 4], 8, true);
     player.animations.add('jump', [5,6,7,8], 5, true );
     // player.animations.add('turn', [4], 20, true);
     // player.animations.add('right', [5, 6, 7, 8], 10, true);
@@ -59,20 +65,25 @@ function create() {
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
+    //chain
+    createChain( 40, 480, 0);
+
 }
 
 function update() {
 
     // game.physics.arcade.collide(player, layer);
+    
+    // reset velocity every tick
+    player.body.velocity.x = 0;
 
-    //modify direction
+    //flip sprite based on direction
     if(facing == 'left'){
         player.scale.x = -2;
     } else if (facing == 'right'){
         player.scale.x = 2;
     }
 
-    player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
     {
@@ -130,4 +141,55 @@ function render () {
     // game.debug.body(player);
     // game.debug.bodyInfo(player, 16, 24);
 
+}
+
+function createChain( length, xAnchor, yAnchor){
+    
+    var lastRect;
+    var height = 20;        //  Height for the physics body - your image height is 8px
+    var width = 16;         //  This is the width for the physics body. If too small the rectangles will get scrambled together.
+    var maxForce = 20000;   //  The force that holds the rectangles together.
+
+    for (var i = 0; i <= length; i++)
+    {
+        var x = xAnchor;                    //  All rects are on the same x position
+        var y = yAnchor + (i * height);     //  Every new rect is positioned below the last
+
+        if (i % 2 === 0)
+        {
+            //  Add sprite (and switch frame every 2nd time)
+            newRect = game.add.sprite(x, y, 'chain', 1);
+        }   
+        else
+        {
+            newRect = game.add.sprite(x, y, 'chain', 0);
+            lastRect.bringToTop();
+        }
+
+        //  Enable physicsbody
+        game.physics.p2.enable(newRect, false);
+
+        //  Set custom rectangle
+        newRect.body.setRectangle(width, height);
+
+        if (i === 0)
+        {
+            newRect.body.static = true;
+        }
+        else
+        {  
+            //  Anchor the first one created
+            newRect.body.velocity.x = 400;      //  Give it a push :) just for fun
+            newRect.body.mass = length / i;     //  Reduce mass for evey rope element
+        }
+
+        //  After the first rectangle is created we can add the constraint
+        if (lastRect)
+        {
+            game.physics.p2.createRevoluteConstraint(newRect, [0, -10], lastRect, [0, 10], maxForce);
+        }
+
+        lastRect = newRect;
+
+    }
 }
